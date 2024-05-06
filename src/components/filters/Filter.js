1775f2bc6322084c678f500df1e2bc2504b71f2e
snekan <snekan13@gmail.com>
2024-05-06 13:58:41 +0530

    filter boxes design

--- src/components/filters/Filter.js
+++ src/components/filters/Filter.js
@@ -1,43 +1,54 @@
-import {fieldValues} from '../../constants/field-values';
-import { GroupedCheckBox,CheckBox } from '../checkbox/CheckBox'; 
-import {filterJobs} from '../../actions/JobActions'
-import {useDispatch} from 'react-redux'
-import {TextField} from '@mui/material'
-import './filter.css'
+import { fieldValues } from "../../constants/field-values";
+import { GroupedCheckBox, CheckBox } from "../checkbox/CheckBox";
+import { filterJobs } from "../../actions/JobActions";
+import { useDispatch } from "react-redux";
+import { TextField } from "@mui/material";
+import "./filter.css";
 
+export default function Filter() {
+  const dispatch = useDispatch();
 
+  const dofilter = (filterKey) => (event, value) => {
+    dispatch(filterJobs(filterKey, value));
+  };
 
-export default  function Filter(){
+  const doCompanySearch = (event) => {
+    console.log("name", event.target.value);
+    dispatch(filterJobs("companyName", event.target.value));
+  };
 
-    const dispatch = useDispatch()
-
-    const dofilter=(filterKey)=>(event,value)=>{
-        dispatch(filterJobs(filterKey,value))
-    }
-
-    const doCompanySearch=(event)=>{
-        console.log("name",event.target.value)
-        dispatch(filterJobs("companyName",event.target.value))
-    }
-
-
-
-
-    return(
-        <div className='filter-component'>
-            <div className='checkbox-grp'>
-            {
-                fieldValues.map(field =>
-                    <div key={field.title}>
-                         {field?.groupBy?<GroupedCheckBox filterKey={field?.filterKey} placeholder={field.title} options={field.options} onChange={dofilter}/>
-                                    :<CheckBox isMultiple={field?.isMultiple} filterKey={field?.filterKey} placeholder={field.title} onChange={dofilter}options={field.options}/>}
-                    </div>
-            )
-            }
-            </div>
-            <div className='text-field'>
-                    <TextField id="outlined-basic" onChange={doCompanySearch} label="search company name"  variant="outlined" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="filter-component">
+      <div className="checkbox-grp">
+        {fieldValues.map((field) => (
+          <div className="filter-box" key={field.title}>
+            {field?.groupBy ? (
+              <GroupedCheckBox
+                filterKey={field?.filterKey}
+                placeholder={field.title}
+                options={field.options}
+                onChange={dofilter}
+              />
+            ) : (
+              <CheckBox
+                isMultiple={field?.isMultiple}
+                filterKey={field?.filterKey}
+                placeholder={field.title}
+                onChange={dofilter}
+                options={field.options}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="text-field">
+        <TextField
+          id="outlined-basic"
+          onChange={doCompanySearch}
+          label="search company name"
+          variant="outlined"
+        />
+      </div>
+    </div>
+  );
 }
--- src/reducers/Job.js
+++ src/reducers/Job.js
@@ -1,64 +1,112 @@
-import {JobActionTypes} from '../constants/action-fields';
+import { JobActionTypes } from "../constants/action-fields";
 
-
-export const Job = (state={jobs:[],actualJobs:[],filter:{jobRole:[],minExp:[],location:[],minJdSalary:"",companyName:""}},{type,payload})=>{
-    switch(type) {
-        case JobActionTypes.GET_JOBS :{
-            console.log("pay",payload)
-            const actualJobs = [...state.actualJobs,...payload.actualJobs];
-            const jobs = getFilteredJob(actualJobs,state.filter)
-            return {...state,jobs:jobs,actualJobs}
-        }
-        case JobActionTypes.FILTER_JOBS :{
-            const filterBy = payload.filterBy;
-            const filterValue = getFormatFilterValues(filterBy,payload.filterValue);
-            console.log("updated filter12",filterBy,filterValue)
-            const filter = {...state.filter,[filterBy]:filterValue};
-            console.log("updated filter",filter)
-            const filterJobs=  getFilteredJob(state?.actualJobs,filter)
-            console.log("filteredJobs",filterJobs)
-            return {...state,jobs:filterJobs,filter:filter}
-        }
-        default : {
-            return state;
-        }
+export const Job = (
+  state = {
+    jobs: [],
+    actualJobs: [],
+    filter: {
+      jobRole: [],
+      minExp: [],
+      location: [],
+      minJdSalary: "",
+      companyName: "",
+    },
+  },
+  { type, payload }
+) => {
+  switch (type) {
+    case JobActionTypes.GET_JOBS: {
+      console.log("pay", payload);
+      const actualJobs = [...state.actualJobs, ...payload.actualJobs];
+      const jobs = getFilteredJob(actualJobs, state.filter);
+      return { ...state, jobs: jobs, actualJobs };
     }
-}
-
-
-const getFilteredJob = (jobs,filter) =>{
-    console.log("hhh",filter)
-    return jobs.filter(job =>{
-        const roleCondition = filter.jobRole.length>0 ? job?.jobRole !=null && filter.jobRole.includes(job?.jobRole) :true;
-        const expCondtion =  filter.minExp.length>0 ? job?.minExp !=null && filter.minExp.includes(job?.minExp) :true;
-        const locationCondition = filter.location.length>0 ? job?.location !=null  && 
-                                (filter.location.includes(job?.location) || (filter.location.includes("in-office") && job?.location !== 'remote' && job?.location!=='hybrid')):true
-
-        const salaryConditon = (filter.minJdSalary !==null && filter.minJdSalary.length>0) ? job?.minJdSalary !=null && parseInt(filter.minJdSalary.replace("L","")) <=job?.minJdSalary :true
-
-        const companyNameCondition = filter.companyName.length >0? job?.companyName != null && job?.companyName.includes(filter.companyName):true
-
-        console.log("roleCondition",roleCondition,"expCondtion",expCondtion,"locationCondition",locationCondition,"salaryConditon",salaryConditon,"com",companyNameCondition)
-        return roleCondition && expCondtion && locationCondition && salaryConditon && companyNameCondition
-    });
-}
-
-
-const getFormatFilterValues=(filterBy,filterValues) => {
-    switch(filterBy) {
-        case "minExp":{
-            return Array.from(filterValues,Number);
-        }
-        case "jobRole" :{
-            return filterValues.map((role)=>role?.name.toLowerCase());
-        }
-        case "location" :{
-            return filterValues.map(location => location.toLowerCase())
-        }
-        
-        default :{
-            return filterValues;
-        }
+    case JobActionTypes.FILTER_JOBS: {
+      const filterBy = payload.filterBy;
+      const filterValue = getFormatFilterValues(filterBy, payload.filterValue);
+      console.log("updated filter12", filterBy, filterValue);
+      const filter = { ...state.filter, [filterBy]: filterValue };
+      console.log("updated filter", filter);
+      const filterJobs = getFilteredJob(state?.actualJobs, filter);
+      console.log("filteredJobs", filterJobs);
+      return { ...state, jobs: filterJobs, filter: filter };
     }
-}
+    default: {
+      return state;
+    }
+  }
+};
 
+const getFilteredJob = (jobs, filter) => {
+  console.log("hhh", filter);
+  return jobs.filter((job) => {
+    const roleCondition =
+      filter.jobRole.length > 0
+        ? job?.jobRole != null && filter.jobRole.includes(job?.jobRole)
+        : true;
+    const expCondtion =
+      filter.minExp.length > 0
+        ? job?.minExp != null && filter.minExp.includes(job?.minExp)
+        : true;
+    const locationCondition =
+      filter.location.length > 0
+        ? job?.location != null &&
+          (filter.location.includes(job?.location) ||
+            (filter.location.includes("in-office") &&
+              job?.location !== "remote" &&
+              job?.location !== "hybrid"))
+        : true;
+
+    const salaryConditon =
+      filter.minJdSalary !== null && filter.minJdSalary.length > 0
+        ? job?.minJdSalary != null &&
+          parseInt(filter.minJdSalary.replace("L", "")) <= job?.minJdSalary
+        : true;
+
+    const companyNameCondition =
+      filter.companyName.length > 0
+        ? job?.companyName != null &&
+          job?.companyName
+            .toLowerCase()
+            .includes(filter.companyName.toLowerCase())
+        : true;
+
+    console.log(
+      "roleCondition",
+      roleCondition,
+      "expCondtion",
+      expCondtion,
+      "locationCondition",
+      locationCondition,
+      "salaryConditon",
+      salaryConditon,
+      "com",
+      companyNameCondition
+    );
+    return (
+      roleCondition &&
+      expCondtion &&
+      locationCondition &&
+      salaryConditon &&
+      companyNameCondition
+    );
+  });
+};
+
+const getFormatFilterValues = (filterBy, filterValues) => {
+  switch (filterBy) {
+    case "minExp": {
+      return Array.from(filterValues, Number);
+    }
+    case "jobRole": {
+      return filterValues.map((role) => role?.name.toLowerCase());
+    }
+    case "location": {
+      return filterValues.map((location) => location.toLowerCase());
+    }
+
+    default: {
+      return filterValues;
+    }
+  }
+};
